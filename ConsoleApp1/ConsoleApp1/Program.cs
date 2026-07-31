c21dcfb18ec9b68b5e34f387006a0eb870842329
Mariane-02 <165634087+Mariane-02@users.noreply.github.com>
2026-08-01 02:55:43 +0800

    Add files via upload
    Signed-off-by: Mariane-02 <165634087+Mariane-02@users.noreply.github.com>

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 
-Console.WriteLine("Hello, World!");
+// 1. Add Session Services
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddDistributedMemoryCache();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+// 2. Add Session Middleware (Must be before MapControllerRoute)
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Auth}/{action=Login}/{id?}");
+
+app.Run();
